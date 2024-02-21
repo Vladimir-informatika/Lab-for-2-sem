180758716c0aa8938dfd26d308abc2693122a904
Vladimir-informatika <159236788+Vladimir-informatika@users.noreply.github.com>
2024-02-21 23:05:53 +0300

    Added coordinates

--- main.cpp
+++ main.cpp
@@ -29,7 +29,17 @@ void input(struct regular_polygon *reg_pol)
     cin >> reg_pol->y;
 }
 
+void square(struct regular_polygon *reg_pol)
+{
+    reg_pol->square = 0.25 * (double)reg_pol->count_sides * reg_pol->length * reg_pol->length * pow(tan(M_PI/((double)reg_pol->count_sides)), -1.0);
+    cout << "Square = " << reg_pol->square << '\n';
+}
 
+void perimeter(struct regular_polygon *reg_pol)
+{
+    reg_pol->perimeter = reg_pol->count_sides * reg_pol->length;
+    cout << "Perimeter = " << reg_pol->perimeter << '\n';
+}
 
 
 int main()
@@ -37,5 +47,20 @@ int main()
     regular_polygon *rg;
     rg = &reg_pol;
     input(rg);
+    perimeter(rg);
+    square(rg);
+    double ang = ((reg_pol.count_sides - 2) * M_PI)/(reg_pol.count_sides);
+    double R = reg_pol.length/(2 * sin(M_PI/(reg_pol.count_sides)));
+    double xO = reg_pol.x + R;
+    double yO = reg_pol.y;
+    for (int i = 0; i < reg_pol.count_sides; i++)
+    {
+        x1 = xO + R * sin(i * ang);
+        y1 = yO + R * cos(i * ang);
+        i++;
+        cout << "x" << x1 << i << '\n';
+        cout << "y" << y1 << i << '\n';
+
+    }
     return 0;
 }
