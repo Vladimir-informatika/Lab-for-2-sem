2d4e66a2099a03f2f48829ea4b7b5bcc6de838a4
denishamenushko <150679026+denishamenushko@users.noreply.github.com>
2024-02-25 19:40:18 +0300

    Add files via upload

--- lab1main.cpp
+++ lab1main.cpp
@@ -2,10 +2,10 @@
 #include "cmath"
 
 using namespace std;
-int count_square(int amount, int length);
-int count_perimetr(int *amount, int *length);
+int count_square(int *amount, int *length);
+int count_perimetr(int &amount, int &length);
 void clean(int var =1);
-
+int menu(int var=1);
 int main()
 {
     clean();
@@ -14,21 +14,21 @@ int main()
     {
         int amount;
         int length;
-        int square=count_square(amount, length);
-        int perimetr=count_perimetr(&amount, &length);
+        int square=count_square(&amount, &length);
+        int perimetr=count_perimetr(amount, length);
         int x;
         int y;
     }poly;
     int flag=1;
     while(flag==1)
     {
-        cout<<"Введите 1, чтобы начать ввод параметров многоугольника\n";
-        cout<<"Введите 0, чтобы выйти из программы\n";
-        int choice=10;
-        cin>> choice;
-        if(choice==0)
+      int choice=10;
+      choice=menu();
+      switch(choice)
         {
-            cout<<"Вы уыерены, что хотите выйти? 0-Да, 1-Продолжить работу\n";
+        case(0):
+        {
+            cout<<"Are you sure you want to exit?\n0-yes\n1-go back\n";
             int exit=2;
             cin>> exit;
             if(exit==0)
@@ -41,32 +41,34 @@ int main()
             }
 
         }
-        if (choice==1)
+        case(1):
         {
-            cout<<"Введите количество сторон:\n";
+            cout<<"amount of angles: ";
             cin>>poly.amount;
-            cout<<"Введите длину стороны:\n";
+            cout<<"\nlength of side: ";
             cin>>poly.length;
             cout<<poly.length<<"\n";
+            cout<<"Perimrtr: ";
             cout<<poly.perimetr<<"\n";
         }
+          default: cout<<"\nError, try another number!\n";
     }
-
+    }
 
     return 0;
 }
-int count_square(int amount, int length)
+int count_square(int *amount, int *length)
 {
     int result=0;
-    result=((amount*length*length)/4)*(cos(180/amount)/sin(180/amount));
+  result = ((*amount * (*length) * (*length)) / 4) * (cos(180 / (*amount)) / sin(180 / (*amount)));
 
    return result;
 }
 
-int count_perimetr(int *amount, int *length)
+int count_perimetr(int &amount, int &length)
 {
     int result;
-    result=*amount**length;
+    result=(amount)*(length);
     return result;
 }
 
@@ -76,4 +78,13 @@ void clean(int var)
     {
         cout<<"\n";
     }
+}
+int menu(int var)
+{
+  cout<<"Inter a number to choose action:\n";
+  cout<<"0 for exit\n";
+  cout<<"1 for adding polynom\n";
+  int choice=10;
+  cin>> choice;
+  return choice;
 }
