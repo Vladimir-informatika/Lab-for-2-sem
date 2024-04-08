5ea9112fd68072085650064ef63bc0dbbd919c66
Vladimir-informatika <159236788+Vladimir-informatika@users.noreply.github.com>
2024-04-08 19:42:50 +0300

    Laba2

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle{
+    public:
+        char name;
+        double mileage;
+        double power;
+        double speed;
+        int count_wheels;
+        double volume;
+        double consumption;
+        double time;
+}; Car
+int main()
+{
+    Vehicle Car;
+
+    return 0;
+}
+
+void time_calc(double distance, double strong){
+    double mass = 1250;
+    double mu = 0.02;
+    strong = mu * mass * 10;
+//    speed = ptr->power / strong;
+//    time = distance / speed;
+//    cout << speed << endl;
+}
