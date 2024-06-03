b9be6aa67269176f9017148e334a83856c59c1b1
Vladimir-informatika <159236788+Vladimir-informatika@users.noreply.github.com>
2024-06-03 13:45:12 +0300

    class almost completed

--- main.cpp
+++ main.cpp
@@ -12,26 +12,60 @@ class Transport
     double power_engine;
     double travel_time;
     string name;
+    double speed;
+    double engine_consumption;
+    int refills;
+    double time;
 public:
-    Transport(int countWheels, double mil, double volumeTank, double power, double time, string n)
-    {
-        count_wheels = countWheels;
-        mileage = mil;
-        volume_tank = volumeTank;
-        power_engine = power;
-        travel_time = time;
-        name = n;
-    }
-    //double CalcEngineConsumption();
-    //double CalcSpeed();
-    //double CalcTime();
-    //int CalcRefills();
-    void Print()
-    {
-        cout << name << travel_time << power_engine << volume_tank << mileage << count_wheels;
-    }
-    ~Transport() { cout << "Deleled object"; };
+    Transport(int countWheels, double mil, double volumeTank, double power, double time, string str);
+
+    double CalcEngineConsumption();
+    double CalcSpeed();
+    double CalcTravelTime();
+    int CalcRefills();
+    void Print();
+
+    ~Transport();
 };
+
+Transport::~Transport() {
+    cout << "Transport " << name << " deleted!" << endl;
+}
+
+Transport::Transport(int countWheels, double mil, double volumeTank, double power, double time1, string str) {
+    count_wheels = countWheels;
+    mileage = mil;
+    volume_tank = volumeTank;
+    power_engine = power;
+    time = time1;
+    name = str;
+
+    speed = CalcSpeed();
+    engine_consumption = CalcEngineConsumption();
+    refills = CalcRefills();
+    travel_time = CalcTravelTime();
+}
+
+double Transport::CalcSpeed() {
+    return 0;
+}
+
+double Transport::CalcEngineConsumption() {
+    return 0;
+}
+
+double Transport::CalcTravelTime() {
+    return 0;
+}
+
+int Transport::CalcRefills() {
+    return 0;
+}
+
+void Transport::Print() {
+    return;
+}
+
 int main()
 {
     Transport Car(3, 5, 5, 5, 5, "car");
